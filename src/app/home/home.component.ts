c89f04bfc6ffb93ef0a430623584d9b83840901a
Kiran Anand <liteninkiran@hotmail.com>
2025-09-05 16:54:29 +0100

    014 - Introduction To The Complete CRUD With Signals Section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, EffectRef, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 
 @Component({
@@ -9,28 +9,5 @@ import { MatTab, MatTabGroup } from '@angular/material/tabs';
     imports: [MatTabGroup, MatTab],
 })
 export class HomeComponent {
-    public counter = signal<number>(0);
-
-    public effectRef: EffectRef | null = null;
-
-    constructor() {
-        this.effectRef = effect((onCleanup) => {
-            const counter = this.counter();
-            const timeout = setTimeout(() => {
-                console.log(`Counter: ${counter}`);
-            }, 1000);
-            onCleanup(() => {
-                console.log('Cleanup');
-                clearTimeout(timeout);
-            });
-        });
-    }
-
-    public increment(): void {
-        this.counter.update((val) => val + 1);
-    }
-
-    public onCleanup(): void {
-        this.effectRef?.destroy();
-    }
+    constructor() {}
 }
