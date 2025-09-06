23425f228a5f0317b5606b35c2c3f8e7f4d2926d
Kiran Anand <liteninkiran@hotmail.com>
2025-09-06 20:28:16 +0100

    017 - Angular HTTP Service Layers - Two Different Options

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
+import { Course } from '../models/course.model';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CoursesServiceWithFetch {
     env = environment;
+
+    public loadAllCourses = async (): Promise<Course[]> => {
+        const response = await fetch(`${this.env.apiRoot}/api/courses`);
+        const payload = await response.json();
+        return payload.courses;
+    };
 }
