4bdaa780cf389138512ed70a16554661fa6ba2c5
Kiran Anand <liteninkiran@hotmail.com>
2025-09-24 18:55:51 +0100

    036 - Signal-Based User Messages - Shared Service

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Message, MessageSeverity } from '../models/message.model';
 
 @Injectable({
     providedIn: 'root',
 })
-export class MessagesService {}
+export class MessagesService {
+    #messageSignal = signal<Message | null>(null);
+    public message = this.#messageSignal.asReadonly();
+
+    public showMessage(text: string, severity: MessageSeverity) {
+        this.#messageSignal.set({ text, severity });
+    }
+
+    public clear() {
+        this.#messageSignal.set(null);
+    }
+}
