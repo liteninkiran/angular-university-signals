9d9c9106a2ea38b0a4ef4712bc6ad927989bd178
Kiran Anand <liteninkiran@hotmail.com>
2025-09-24 18:58:42 +0100

    037 - Messages Component - Step-by-Step Implementation

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MessagesService } from './messages.service';
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'messages',
     templateUrl: './messages.component.html',
     styleUrls: ['./messages.component.scss'],
+    imports: [NgClass],
 })
-export class MessagesComponent {}
+export class MessagesComponent {
+    public messagesService = inject(MessagesService);
+    public message = this.messagesService.message;
+    public src = '/assets/icons/close.svg';
+
+    public onClose(): void {
+        this.messagesService.clear();
+    }
+}
