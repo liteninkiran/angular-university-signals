77d5d56032f085c646de0e319809941d37347e38
Kiran Anand <liteninkiran@hotmail.com>
2025-09-27 16:48:55 +0100

    054 - Master Detail UI Pattern

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Lesson } from '../models/lesson.model';
+import { LessonsService } from '../services/lessons.service';
+
+type Type = 'master' | 'detail';
 
 @Component({
     selector: 'lessons',
     templateUrl: './lessons.component.html',
     styleUrl: './lessons.component.scss',
 })
-export class LessonsComponent {}
+export class LessonsComponent {
+    public mode = signal<Type>('master');
+    public lessons = signal<Lesson[]>([]);
+    public selectedLesson = signal<Lesson | null>(null);
+
+    public lessonsService = inject(LessonsService);
+}
