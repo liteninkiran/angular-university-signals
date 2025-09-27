751881ca19d1876df964c1ebb87934784eeabf4e
Kiran Anand <liteninkiran@hotmail.com>
2025-09-27 16:10:58 +0100

    052 - Implementing the Lessons Service

--- src/app/services/lessons.service.ts
+++ src/app/services/lessons.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { GetLessonsResponse } from '../models/get-lessons.response';
+import { firstValueFrom } from 'rxjs';
+import { Lesson } from '../models/lesson.model';
+
+type Config = {
+    courseId?: string;
+    query?: string;
+};
 
 @Injectable({
     providedIn: 'root',
 })
 export class LessonsService {
-    env = environment;
+    private env = environment;
+    private http = inject(HttpClient);
+
+    public loadLessons = async (config: Config): Promise<Lesson[]> => {
+        const params = this.setParams(config);
+        const url = `${this.env.apiRoot}/search-lessons`;
+        const lessons$ = this.http.get<GetLessonsResponse>(url, { params });
+        const response = await firstValueFrom(lessons$);
+        return response.lessons;
+    };
+
+    private setParams(config: Config): HttpParams {
+        const { courseId, query } = config;
+        let params = new HttpParams();
+
+        if (courseId) {
+            params = params.set('courseId', courseId);
+        }
+        if (query) {
+            params = params.set('query', query);
+        }
+        return params;
+    }
 }
