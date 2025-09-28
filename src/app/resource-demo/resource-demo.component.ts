290a8c81f71e5b8f7f3e430f5e4567c8c345cc06
Kiran Anand <liteninkiran@hotmail.com>
2025-09-28 15:39:41 +0100

    067 - Understanding the Angular resource() API

--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -1,19 +1,48 @@
-import { Component, signal } from '@angular/core';
+import {
+    Component,
+    effect,
+    resource,
+    ResourceLoaderParams,
+    signal,
+} from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Lesson } from '../models/lesson.model';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+
+type Search = {
+    search: string;
+};
 
 @Component({
     selector: 'resource-demo',
     templateUrl: './resource-demo.component.html',
     styleUrls: ['./resource-demo.component.scss'],
+    standalone: true,
+    imports: [MatProgressSpinner],
 })
 export class ResourceDemoComponent {
     private env = environment;
 
     public search = signal<string>('');
-    public lessons = signal<Lesson[]>([]);
+    public lessons = resource<Lesson[], Search>({
+        params: () => ({
+            search: this.search(),
+        }),
+        loader: async (loaderParams: ResourceLoaderParams<Search>) => {
+            const { abortSignal, params } = loaderParams;
+            const url = `${this.env.apiRoot}/search-lessons?query=${params?.search}&courseId=18`;
+            const options = { signal: abortSignal };
+            const response = await fetch(url, options);
+            const json = await response.json();
+            return json.lessons;
+        },
+    });
 
-    constructor() {}
+    constructor() {
+        effect(() => {
+            console.log('Searching Lessons', this.search());
+        });
+    }
 
     public searchLessons(search: string): void {
         this.search.set(search);
